ba7f1b94291c7a17dff2d28112eb9da475003b36
kairusds <harveyhansjavier@gmail.com>
2019-03-13 08:18:25 +0800

    cataclysmic ordeal

--- commands/passgen.js
+++ commands/passgen.js
@@ -3,29 +3,26 @@ const {stripIndents} = require("common-tags");
 
 module.exports = {
 	name: "passgen",
-	format: "[length=15, amount=1]",
+	format: "[length=15, symbols=false, amount=1]",
 	description: "Generate strong password(s).",
 	run: async (client, message, args) => {
-		const length = Number(args[0]) || 15;
-		const amount = Number(args[1]) || 1;
+		const amount = Number(args[2]) || 1;
+		const options = {
+			length: Number(args[0]) || 16,
+			numbers: true,
+			symbols: Boolean(args[1]),
+			excludeSimilarCharacters: true,
+			strict: true
+		};
+		
 		if(amount > 1){
-			const passwords = generateMultiple(amount, {
-				length,
-				numbers: true,
-				excludeSimilarCharacters: true,
-				strict: true
-			});
+			const passwords = generateMultiple(amount, options);
 			return message.edit(stripIndents`||
 			${passwords.join("\n")}
 			||`);
 		}
 		
-		const password = generate({
-			length,
-			numbers: true,
-			excludeSimilarCharacters: true,
-			strict: true
-		});
+		const password = generate(options);
 		return message.edit(`||${password}||`);
 	}
 };
